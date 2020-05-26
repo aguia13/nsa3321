3c5ff30680ac9c1fcfa8eeac9a49a153319a7037
aguia13 <aguia13@wfu.edu>
2020-05-25 23:54:22 -0500

    Add files via upload

--- HW/OOP.py
+++ HW/OOP.py
@@ -2,7 +2,7 @@ from csv import DictReader
 import io
 
 class Symbol(object):
-	def __init__(self,name=None,input_file=None):
+	def __init__(self,name,input_file):
 		self.name = name
 		self.input_file = input_file
 		self.daily_data = {}
@@ -13,14 +13,15 @@ class Symbol(object):
 					record = str.split(line,",")
 					self.daily_data[record[0]] = [record[1],record[2],record[3],record[4],record[5]]
 					#print(record)
-
 		except:
-			print("NOPE")
+			return self.daily_data
 
 	def data_for_date(self,date_str):
 		if date_str in self.daily_data:
 			print(self.daily_data[date_str])
-				
+
+		else:
+			return {}
 
 
 
@@ -32,9 +33,9 @@ class Symbol(object):
 def main():
 	s1 = Symbol('AAPL','aapl.csv')
 	print(s1.data_for_date('18-Feb-16'))
-
-	return	
-
+	#print(s1.data_for_date('18-Feb-20'))
+	
+	#s2 = Symbol('AAPL','aaple.csv')
 
 
 if __name__ == '__main__':
